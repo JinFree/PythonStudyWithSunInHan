4b3dd47620787d4821ae14b1a9f5680b5f9699d2
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-11 00:10:37 +0900

    1st week

---
+++ python_basic_02.py
@@ -0,0 +1,39 @@
+#data type
+my_int1 = 1
+my_int2 = -2
+my_int3 = 4096
+
+my_float1 = 1.0
+my_float2 = 2.0
+my_float3 = 3.14
+
+my_str1 = 'a'
+my_str2 = '3.14'
+my_str3 = 'coding'
+my_str4 = "coding"
+
+my_bool1 = True  # True
+my_bool2 = False  # False
+my_bool3 = 1 < 2  # True
+my_bool4 = 1 == 2  # False
+
+my_list = []  # []
+my_list.append(123)  # [123]
+my_list.append('abc') # [123, 'abc']
+my_list.append(True)  # [123, 'abc', True]
+
+my_tuple1 = ()
+my_tuple2 = (1,)
+my_tuple3 = ('a', 'b', 'c')
+my_tuple4 = 3.14, 'Python', True
+
+my_dict = {}  # {}
+my_dict[1] = 'a'  # {1: 'a'}
+my_dict['b'] = 2  # {1: 'a', 'b': 2}
+my_dict['c'] = 'd'  # {1: 'a', 'b': 2, 'c': 'd'}
+
+print(int(3.14))
+print(float(3))
+print(str(3.0))
+print(type(str(3.0)))
+print(list('3.0'))
