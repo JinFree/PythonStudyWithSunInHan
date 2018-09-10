383bd7260e049f038504760ce6478ef865af2e0c
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-11 00:06:35 +0900

    1st week

---
+++ python_basic_01.py
@@ -0,0 +1,13 @@
+print()
+print('Hello world!')
+print(1)
+print([1, 2, 3])
+print('python')
+print('My name is', 'JY')
+name = input('What is your name?: ')
+print(name)
+my_int = 1
+my_str = 'Python'
+count = 1  # 1
+count = count + 1  # 2
+print(count)
