84f63919249dabf10f7087c96e507e9dc932f3d9
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-16 11:04:48 +0900

    pythonstudy

---
+++ python_basic_03.py
@@ -0,0 +1,37 @@
+my_str = 'python'
+print(my_str)
+type(my_str)
+
+print("Let`s go!")
+print('I said, "Hello!"')
+
+print('My name is %s' % 'Tom')
+print('x = %d, y = %d' % (1, 2))
+print('%d x %d = %d' % (2, 3, 2 * 3))
+print('%d %d' % (1, 2))
+print('%f' % 3.14)
+print('%s' % 'coding')
+
+print('My name is %s' % 'Tom')
+print('My name is {}'.format('Tom'))
+print('{} x {} ={}'.format(2, 3, 2 * 3))
+print('{1} x {0} ={2}'.format(2, 3, 2 * 3))  
+
+alphabet = 'abcde'
+print(alphabet[0])
+print(alphabet[3])
+
+my_name = '김왼손의 왼손코딩'
+print(my_name[3])
+print(my_name[8])
+print(my_name[-1])
+
+my_str = 'Hello Python!'
+print(my_str[0:1])
+print(my_str[0:2])
+print(my_str[3:7])
+
+my_name = '김왼손의 왼손코딩'
+print(my_name[5:7])
+print(my_name[:2])
+print(my_name[7:])
---
+++ python_basic_04.py
@@ -0,0 +1,23 @@
+fruit_str = 'apple banana lemon'
+fruits = fruit_str.split()
+print(fruits[0])
+print(fruits[1])
+
+""" 이것은 주석, 독스트링"""
+print(' ', end='')
+print('집단지성', end='/')
+print('집단지성', end='미운코딩새끼')
+
+print("")
+my_list = []
+print(type(my_list))
+my_list = [1, 2, 3]
+print(my_list)
+
+students = ['Tom', 'Alice', 'Sally']
+students.append('Betty')
+students.append('Angela')
+print(students)
+
+animals = ['코알라', '하이에나', '바다소', '땅다람쥐', '바다코끼리', '스컹크', '아나콘다']
+print(animals[1])
